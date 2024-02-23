4c2d9be6a4ea1095a4a681278b776aab7250bdf9
Sudheer T <tvsudhir3@gmail.com>
2024-02-23 15:57:28 -0600

    feat: Setup Jest test-cases using babel (cumbersome otherwise). Set some basic code coverage thresholds.

---
+++ __tests__/todos/createToDo.js
@@ -0,0 +1,17 @@
+import { jest, describe, test, it } from '@jest/globals';
+import createToDo from '../../src/todos/createTodo.js';
+import * as postgres from '../../src/datastore/postgres.js';
+
+jest.mock('../../src/datastore/postgres.js');
+
+it('checking', async () => {
+  postgres.pSql.mockResolvedValue(
+    [
+      { todoid: 'td1', name: 'test-1', text: 'some text' }
+    ]
+  );
+  
+  const result = await createToDo({ todId: 'td1', name: 'test-1', text: 'some text' });
+
+  expect(result).toEqual({ todoId: 'td1', name: 'test-1', text: 'some text' });
+});
