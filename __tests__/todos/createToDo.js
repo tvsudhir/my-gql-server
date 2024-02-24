63ddc849f3e26241a10a62d042e4ebe392f84b30
Sudheer T <tvsudhir3@gmail.com>
2024-02-24 11:52:44 -0600

    fix: Minor ESLint adjustments.

--- __tests__/todos/createToDo.js
+++ __tests__/todos/createToDo.js
@@ -1,4 +1,4 @@
-import { jest, describe, test, it } from '@jest/globals';
+import { jest, it, expect } from '@jest/globals';
 import createToDo from '../../src/todos/createTodo.js';
 import * as postgres from '../../src/datastore/postgres.js';
 
@@ -7,10 +7,10 @@ jest.mock('../../src/datastore/postgres.js');
 it('checking', async () => {
   postgres.pSql.mockResolvedValue(
     [
-      { todoid: 'td1', name: 'test-1', text: 'some text' }
-    ]
+      { todoid: 'td1', name: 'test-1', text: 'some text' },
+    ],
   );
-  
+
   const result = await createToDo({ todId: 'td1', name: 'test-1', text: 'some text' });
 
   expect(result).toEqual({ todoId: 'td1', name: 'test-1', text: 'some text' });
